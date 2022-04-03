e877f43a9f74a38490d34c4bf5913bb7290a3f56
Bryan Yao <reccepython@gmail.com>
2022-04-03 13:19:40 +0800

    Aviation Occurence AIClassifier

--- Aviation_Occ_AIClassifier.py
+++ Aviation_Occ_AIClassifier.py
@@ -29,7 +29,7 @@ Determines if an event is an "Incident" or "Serious Incident"
 # Dataset
 X_predict = pd.DataFrame()
 data = pd.DataFrame()
-data = pd.read_excel("C:/Users/BRYAN SIOW/Desktop/Python Pi/Github/Aviation_Occurrence_AIClassifier/CaseDatabase.xlsx")
+data = pd.read_excel("https://github.com/reccepython/Aviation_Occurrence_AIClassifier/blob/main/CaseDatabase.xlsx")
 data.drop('No', axis=1, inplace=True)
 X = data.iloc[:,:-2].values
 Y = data['Inc_SInc']
